7b7b68cf811c02bcb78b127545dc3e2f49cf9143
azjf <a@z.jf>
2019-01-27 17:17:43 +0800

    Apply dialog box theme to the about box

--- app/src/main/java/com/example/sudoku/AboutActivity.java
+++ app/src/main/java/com/example/sudoku/AboutActivity.java
@@ -1,9 +1,9 @@
 package com.example.sudoku;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
