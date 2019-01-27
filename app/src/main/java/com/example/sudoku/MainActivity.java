778afb7088cf11724f0da1de8504b5de7d489e75
x <x@x.com>
2019-01-27 17:07:47 +0800

    Implement an about box

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.sudoku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View continueButton = this.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = this.findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = this.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = this.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.about_button:
+                Intent i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
+
 }
