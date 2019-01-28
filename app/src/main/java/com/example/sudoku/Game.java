5a1733098aca0aa28b61c3c26cb8fc4b61a6b52a
x <x@x.com>
2019-01-28 21:53:32 +0800

    Define the Game class

---
+++ app/src/main/java/com/example/sudoku/Game.java
@@ -0,0 +1,33 @@
+package com.example.sudoku;
+
+import android.app.Activity;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TableRow;
+
+public class Game extends Activity {
+    private static final String TAG = "Sudoku";
+
+    public static final String KEY_DIFFICULTY = "difficulty";
+    public static final int DIFFICULTY_EASY = 0;
+    public static final int DIFFICULTY_MEDIUM = 1;
+    public static final int DIFFICULTY_HARD = 2;
+
+    private int puzzle[] = new int[9 * 9];
+    //private PuzzleView puzzleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        int diff = getIntent().getIntExtra(KEY_DIFFICULTY, DIFFICULTY_EASY);
+        //puzzle = getPuzzle(diff);
+        //calculateUsedTiles();
+
+        //puzzleView = new PuzzleView(this);
+        //setContentView(puzzleView);
+        //puzzleView.requestFocus();
+    }
+}
